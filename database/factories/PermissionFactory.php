a761e0649e0a12b2ff69d784c6c16578bf8aad75
tri-suli <trisulipras@gmail.com>
2024-07-06 11:57:43 +0700

    Initiate spatie role permission lib

---
+++ database/factories/PermissionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Permission;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Permission>
+ */
+class PermissionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'       => $this->faker->jobTitle,
+            'guard_name' => 'web',
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +17,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
+        $admin = User::factory()->create([
+            'name'  => 'admin',
+            'email' => 'admin@eduspark.com',
+        ]);
+
+        $roles = Role::factory()->createMany([
+            ['name' => 'admin'],
+            ['name' => 'user'],
+        ]);
+
+        $permissions = Permission::factory()->createMany([
+            ['name' => 'read'],
+            ['name' => 'write'],
         ]);
     }
 }
